a5d40ec00d80b5c5f4170e01973cc030a863ee69
spidercatnat <natalieb@hey.com>
2020-12-02 08:44:11 -0800

    Adds console clearing

--- secret-santa.js
+++ secret-santa.js
@@ -26,29 +26,21 @@ let results = {};
 let pairing;
 let mismatch = 0;
 while (unassigned_santa.length) {
-  // Reset pairing process
   pairing = true;
-  // Select an secretSanta
   const secretSanta = unassigned_santa[0];
-  // Execute pairing process
   while (pairing) {
-    // Pick a random index
     let randomIndex = Math.floor(Math.random() * unassigned_receiver.length);
-    // Pick a random person according to the random index
     let giftReceiver = unassigned_receiver[randomIndex];
     if (
       giftReceiver.name !== secretSanta.name &&
       giftReceiver.name !== secretSanta.partner
     ) {
-      // move giver from unassigned_santa to assigned_santa
       assigned_santa.push(unassigned_santa.shift());
-      // move gift receiver to assigned_to_Receive
       unassigned_receiver.splice(randomIndex, 1);
       assigned_receiver.push(giftReceiver);
       results[secretSanta.name] = giftReceiver;
       pairing = false;
     } else {
-      // If this part of the loop has occurred more than 6 times, than restart the entire pairing process.
       mismatch++;
       if (mismatch > 6) {
         unassigned_santa = [Raul, Carla, Meg, Aidan, Natalie, Maxine];
@@ -67,24 +59,39 @@ const prompts = [];
 let i = 1;
 for (const santa in results) {
   const message = `Ok, ${santa}, you're up! Make sure nobody's looking. Are you ready? Hit that return key!`;
-  prompts.push({
-    name: santa,
-    type: "confirm",
-    message: i % 2 ? chalk.green.bold(message) : chalk.red.bold(message),
-  });
+  prompts.push(
+    {
+      name: santa,
+      type: "confirm",
+      message: i % 2 ? chalk.green.bold(message) : chalk.red.bold(message),
+    },
+    {
+      name: "intermission",
+      type: "confirm",
+      message:
+        i % 3
+          ? chalk.green.bold("Write it down and hit enter to prepare it for the next santa!")
+          : chalk.red.bold("Write it down and hit enter to prepare it for the next santa!"),
+    }
+  );
   i++;
 }
 
 (async () => {
   for (const p in prompts) {
-    const santa_prompt = prompts[p];
-    const santa_name = prompts[p].name;
-    const result = results[santa_name].name;
-    const response = await prompt(santa_prompt);
-    console.log(
-      chalk.white.bold(
-        `\n \n ${santa_name} has been elected santa for ${result}! \n \n`
-      )
-    );
+    if (prompts[p].name === "intermission") {
+      await prompt(prompts[p])
+      console.clear();
+    } else {
+      const santa_prompt = prompts[p];
+      const santa_name = prompts[p].name;
+      const result = results[santa_name].name;
+      const response = await prompt(santa_prompt);
+      console.log(
+        chalk.white.bold(
+          `\n \n ${santa_name} has been elected santa for ${result}! \n \n`
+        )
+      );
+    }
   }
 })();
